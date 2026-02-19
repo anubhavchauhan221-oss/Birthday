e93007a68e00e2a8eab0c6588be4c6781fed6d31
anubhavchauhan221-oss <anubhavchauhan221@gmail.com>
2026-02-19 12:01:10 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // Replace 'Birthday' with your exact repository name if it's different
-  base: '/Birthday/', 
+  base: './Birthday/', 
   plugins: [react()],
 })
